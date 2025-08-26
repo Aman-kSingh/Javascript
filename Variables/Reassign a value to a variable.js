99d48c9afa5f16e1a8d7a65ad704b439b08a928a
Aman Singh <aman60395@gmail.com>
2025-08-26 08:49:36 +0530

    Create Reassign a value to a variable.js

---
+++ Variables/Reassign a value to a variable.js
@@ -0,0 +1,4 @@
+//Reassign a value to a variable
+let num = 1;
+num = 2;    //there's no let keyword here - we're not declaring a new variable, just changing the value of the existing one
+console.log(num);
